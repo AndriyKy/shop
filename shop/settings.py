7b440eaae088cd49d3daddd275c227509d71c698
AndriyKy <akrechovetskiy@gmail.com>
2023-01-30 22:51:38 +0200

    feat: Add jQuery to installed apps

--- shop/settings.py
+++ shop/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "jquery",
     "rest_framework",
     "debug_toolbar",
     "crispy_forms",
