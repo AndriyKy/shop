659f7e54d71e7e7894ec90b06c78ef8d81f1348c
AndriyKy <akrechovetskiy@gmail.com>
2023-01-30 22:20:45 +0200

    reg: Register admin panel

--- shop_service/admin.py
+++ shop_service/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
 
-# Register your models here.
+from shop_service.models import Product, Category, Order
+
+admin.site.register(get_user_model())
+
+
+admin.site.register(Category)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    search_fields = ("category",)
+
+
+admin.site.register(Order)
