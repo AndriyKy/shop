a3b5163ca1032805922a91e07b9dccf7b95db68a
AndriyKy <akrechovetskiy@gmail.com>
2023-02-02 11:55:01 +0200

    test: Add tests for shop_api app

--- shop_api/tests.py
+++ shop_api/tests.py
@@ -1,33 +1,83 @@
+from typing import OrderedDict
+
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 from rest_framework.reverse import reverse
 from rest_framework.test import APIClient
 from rest_framework import status
 
-from shop_service.models import Category, Product
+from shop_service.models import Category, Product, Order
 
 SHOP_API_URL = reverse("shop_api:order-list")
+
+
+def _ordered_dict_to_str(ordered_dict: OrderedDict):
+    price = float(ordered_dict["price"])
+    products_str = (
+        f"{ordered_dict['category']}:"
+        f" {ordered_dict['name']} ({price if price % 1 != 0 else int(price)})"
+    )
+    return products_str
 
 
 class ShopAPITests(TestCase):
     def setUp(self):
         self.client = APIClient()
         self.user = get_user_model().objects.create_user(
-            "admin@admin.com", "test_pass", is_staff=True
+            "admin", "test_pass", is_staff=True
         )
         self.client.force_authenticate(self.user)
         self.category = Category.objects.create(name="Laptop")
-        self.product = Product.objects.create(
+        self.product1 = Product.objects.create(
             name="HP", category=self.category, user=self.user, price=780.5
         )
 
-    def test_order_creation(self):  # TODO: complete tests
+        self.product2 = Product.objects.create(
+            name="LG", category=self.category, user=self.user, price=580
+        )
+        self.order = Order.objects.create(
+            name="Test2", email="noreply@gmail.com"
+        )
+        self.order.product.set([self.product2])
+
+    def test_order_creation(self):
         payload = {
             "name": "Test",
             "email": "test@gmail.com",
-            "product": [self.product.id],
+            "products": [self.product1.id],
         }
 
-        response = self.client.post(SHOP_API_URL, data=payload)
-        print(response.data)
+        response = self.client.post(SHOP_API_URL, data=payload, format="json")
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data["name"], payload["name"])
+        self.assertEqual(response.data["email"], payload["email"])
+
+        products = response.data["products"][0]
+        products_str = _ordered_dict_to_str(products)
+        self.assertEqual(str(self.product1), products_str)
+
+    def test_order_creation_error(self):
+        non_existent_index = 100
+        payload = {
+            "name": "Test",
+            "email": "test@gmail.com",
+            "products": [non_existent_index],
+        }
+
+        response = self.client.post(SHOP_API_URL, data=payload, format="json")
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(
+            response.data["non_field_errors"][0],
+            f"Product index {non_existent_index} does not exist!",
+        )
+
+    def test_order_list(self):
+        response = self.client.get(SHOP_API_URL)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        products = response.data[0].pop("products")[0]
+        products_str = _ordered_dict_to_str(products)
+        self.assertEqual(str(self.product2), products_str)
+
+        for key in response.data[0].keys():
+            self.assertEqual(response.data[0][key], getattr(self.order, key))
