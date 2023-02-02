95a3780c280ad875ae3b0b292f1b99e77015b83d
AndriyKy <akrechovetskiy@gmail.com>
2023-02-02 11:53:22 +0200

    feat: Add WhiteNoise to serve staticfiles in prod

--- shop/settings.py
+++ shop/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -145,6 +146,8 @@ STATICFILES_DIRS = [
         "static",
     )
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
